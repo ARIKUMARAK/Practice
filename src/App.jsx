a86d640fc7605e304daeaa75a7c2e33f8295cb21
Arikumar <arikumarak8825@gmail.com>
2026-01-06 10:35:46 +0530

    API

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import './App.css'
 import State from './Components/State'
 import Effect from './Components/Effect'
+import Api from './Components/API'
 
 export default function App() {
   return (
     <div>
       <State></State>
       <Effect></Effect>
+      <Api></Api>
     </div>
   )
 }
